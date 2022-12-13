fa110477824d6e3b50e07145176fe2916ff359f1
TalhaShakel <shakeeltalha637@gmail.com>
2022-12-13 18:35:31 +0500

    beta started

--- lib/main.dart
+++ lib/main.dart
@@ -20,8 +20,15 @@ import 'package:dating_app/Widgets/Background.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+// ...
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
